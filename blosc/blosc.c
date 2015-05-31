aa29ba349202445b9e9dbf110416bf0463babe34
Jack Pappas <jack-pappas@users.noreply.github.com>
2015-05-31 14:50:24 -0400

    Replaced the Git pthreads emulation layer for win32 with the 'winpthreads' header-only library from Lockless, which uses some simpler, faster APIs available in newer version of Windows (Vista and above).

--- blosc/blosc.c
+++ blosc/blosc.c
@@ -50,8 +50,7 @@
 #endif  /* _WIN32 */
 
 #if defined(_WIN32) && !defined(__GNUC__)
-  #include "win32/pthread.h"
-  #include "win32/pthread.c"
+  #include "win32/winpthreads.h"
 #else
   #include <pthread.h>
 #endif
